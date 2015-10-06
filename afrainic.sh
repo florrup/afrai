735a7b6e915541f297a360302083b765a8fffdfd
Flor <florencia441@hotmail.com>
2015-10-06 08:13:45 -0300

    Cambia nombre de variables

--- afrainic.sh
+++ afrainic.sh
@@ -3,7 +3,7 @@
 
 GRALOG="./gralog.sh"   
 
-CNF=$PWD/"prue.txt" 	# DE PRUEBA usar file de configuración
+CNF=CONF/"AFRAINST.conf" 	# DE PRUEBA usar file de configuración
 
 function msjLog() {
   local MOUT=$1
@@ -44,7 +44,7 @@ function noPermisos() {
 # Devuelve 1 si ya fue inicializado, 0 sino
 function verificarAmbienteInicializado() {
   i=0
-  variables=("$GRP" "$CON" "$BIN" "$MAE" "$DATASIZE" "$ACEP" "$RECH" "$PRO" "$REP" "$NOV" "$LOG" "$LOGSIZE")
+  variables=("$GRUPO" "$CONFDIR" "$BINDIR" "$MAEDIR" "$DATASIZE" "$ACEPDIR" "$RECHDIR" "$PROCDIR" "$REPODIR" "$NOVEDIR" "$LOGDIR" "$LOGSIZE")
   for VAR in "${variables[@]}"
   do
     if [[ ! -z "$VAR" ]]; then # si la variable no esta vacia es porque fue inicializado
@@ -143,17 +143,17 @@ function verificarPermisos() {
 
 # Desde el archivo de configuración tomo todas las variables
 function setearVariablesAmbiente() {
-  GRP=$(grep '^GRUPO' $CNF | cut -d '=' -f 2)
-  CON=$(grep '^CONFDIR' $CNF | cut -d '=' -f 2)
-  BIN=$(grep '^BINDIR' $CNF | cut -d '=' -f 2)
-  MAE=$(grep '^MAEDIR' $CNF | cut -d '=' -f 2)
+  GRUPO=$(grep '^GRUPO' $CNF | cut -d '=' -f 2)
+  CONFDIR=$(grep '^CONFDIR' $CNF | cut -d '=' -f 2)
+  BINDIR=$(grep '^BINDIR' $CNF | cut -d '=' -f 2)
+  MAEDIR=$(grep '^MAEDIR' $CNF | cut -d '=' -f 2)
   DATASIZE=$(grep '^DATASIZE' $CNF | cut -d '=' -f 2)
-  ACEP=$(grep '^ACEPDIR' $CNF | cut -d '=' -f 2)
-  RECH=$(grep '^RECHDIR' $CNF | cut -d '=' -f 2)
-  PRO=$(grep '^PROCDIR' $CNF | cut -d '=' -f 2)
-  REP=$(grep '^REPODIR' $CNF | cut -d '=' -f 2)
-  NOV=$(grep '^NOVEDIR' $CNF | cut -d '=' -f 2)
-  LOG=$(grep '^LOGDIR' $CNF | cut -d '=' -f 2)
+  ACEPDIR=$(grep '^ACEPDIR' $CNF | cut -d '=' -f 2)
+  RECHDIR=$(grep '^RECHDIR' $CNF | cut -d '=' -f 2)
+  PROCDIR=$(grep '^PROCDIR' $CNF | cut -d '=' -f 2)
+  REPODIR=$(grep '^REPODIR' $CNF | cut -d '=' -f 2)
+  NOVEDIR=$(grep '^NOVEDIR' $CNF | cut -d '=' -f 2)
+  LOGDIR=$(grep '^LOGDIR' $CNF | cut -d '=' -f 2)
   LOGSIZE=$(grep '^LOGSIZE' $CNF | cut -d '=' -f 2)
 
   # falta setear PATH
@@ -162,23 +162,23 @@ function setearVariablesAmbiente() {
 # Inicializa el ambiente
 function inicializarAmbiente() {
   # permito que todas las variables sean utilizadas desde otros scripts con export
-  export GRP
-  export CON
-  export BIN
-  export MAE
+  export GRUPO
+  export CONFDIR
+  export BINDIR
+  export MAEDIR
   export DATASIZE
-  export ACEP
-  export RECH
-  export PRO
-  export REP
-  export NOV
-  export LOG
+  export ACEPDIR
+  export RECHDIR
+  export PROCDIR
+  export REPODIR
+  export NOVEDIR
+  export LOGDIR
   export LOGSIZE
 }
 
 # Muestra y graba en el log variables y contenido
 function mostrarYgrabar() {
-  variables=("$CON" "$BIN" "$MAE" "$NOV" "$ACEP" "$PRO" "$REP" "$LOG" "$RECH")
+  variables=("$CONFDIR" "$BINDIR" "$MAEDIR" "$NOVEDIR" "$ACEPDIR" "$PROCDIR" "$REPODIR" "$LOGDIR" "$RECHDIR")
   mensajes=("Configuración" "Ejecutables" "Maestros y Tablas" "Recepción de archivos de llamadas" "Archivos de llamadas Aceptadas" "Archivos de llamadas Sospechosas" "Archivos de Reportes de llamadas" "Archivos de Log" "Archivos Rechazados")
   i=0
   for VAR in "${variables[@]}"
@@ -186,7 +186,7 @@ function mostrarYgrabar() {
     MSJ="Directorio de ""${mensajes[${i}]}":" $VAR"
     msjLog "$MSJ" "INFO"
     # listar archivos si es CONFDIR, BINDIR, MAEDIR, LOGDIR
-    if [ "$VAR" = "$CON" ] || [ "$VAR" = "$BIN" ] || [ "$VAR" = "$MAE" ] || [ "$VAR" = "$LOG" ] ; then
+    if [ "$VAR" = "$CONFDIR" ] || [ "$VAR" = "$BINDIR" ] || [ "$VAR" = "$MAEDIR" ] || [ "$VAR" = "$LOGDIR" ] ; then
       LIST=$(ls "$VAR")
       msjLog "$LIST" "INFO"
     fi
@@ -205,7 +205,8 @@ function deseaArrancar() {
       echo "NOOO" # falta explicar como hacerlo con comando arrancar
       ;;
     "si")
-      echo "SIIII" # falta activar demonio
+      echo "SIIII"
+      $BINDIR/arrancar.sh afrareci  	 # falta activar demonio
       ;;
     *)
       echo "La respuesta debe ser \"si\" o \"no\""
@@ -229,18 +230,18 @@ fi
 setearVariablesAmbiente
 
 # 2. Verifica instalacion completa
-CDP="${MAE}"/"CdP.mae"			# agregar "${BINDIR}"
-CDA="${MAE}"/"CdA.mae"
-CDC="${MAE}"/"CdC.mae"
-AGE="${MAE}"/"agentes.mae"
-TLL="${MAE}"/"tllama.tab"
-UMB="${MAE}"/"umbral.tab"
+CDP="${MAEDIR}"/"CdP.mae"			# agregar "${BINDIR}"
+CDA="${MAEDIR}"/"CdA.mae"
+CDC="${MAEDIR}"/"CdC.mae"
+AGE="${MAEDIR}"/"agentes.mae"
+TLL="${MAEDIR}"/"tllama.tab"
+UMB="${MAEDIR}"/"umbral.tab"
 
-AFRARECI="afrareci.sh"
-AFRAUMBR="afraumbr.sh"
-AFRALIST="afralist.pl"
-ARRANCAR="arrancar.sh"
-DETENER="detener.sh"
+AFRARECI="${BINDIR}"/"afrareci.sh"
+AFRAUMBR="${BINDIR}"/"afraumbr.sh"
+AFRALIST="${BINDIR}"/"afralist.pl"
+ARRANCAR="${BINDIR}"/"arrancar.sh"
+DETENER="${BINDIR}"/"detener.sh"
 
 archivos=("$CDP" "$CDA" "$CDC" "$AGE" "$TLL" "$UMB")
 scripts=("$AFRARECI" "$AFRAUMBR" "$AFRALIST" "$ARRANCAR" "$DETENER")
